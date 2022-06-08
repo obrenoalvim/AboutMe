72cb8013ca3ae6619c2fa4d4d98845240a78e23c
Breno Alvim <alvimbrenosilva6@gmail.com>
2022-06-07 22:10:56 -0300

    delete meta tag

--- src/components/Head/Meta.tsx
+++ src/components/Head/Meta.tsx
@@ -6,7 +6,7 @@ export default function Meta() {
       <Head>
         <meta content="pt-br" />
 
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta content="IE=edge" />
 
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 

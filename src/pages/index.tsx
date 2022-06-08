87fcb74bc91470c087044c76780cb3b59bec4236
Breno Alvim <alvimbrenosilva6@gmail.com>
2022-06-07 21:54:17 -0300

    new file global.css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import "./global.css";
+
 import Meta from "../components/Head/Meta";
 
 import Link from "../components/Head/Link";
@@ -18,21 +20,7 @@ export default function Home() {
       <Title name="About Me" />
       <Header name="</>" />
       <Homepage name="Home" path_img="img/login.png" />
-      <Social name="Breno"/>
-      {/* <style>
-        {`
-        body {
-          margin: 0;
-          padding: 0;
-        }
-        :root {
-          --brand-color: #152d73;
-          --dark-color: #0f172a;
-          --mid-color: #cbd5e1;
-          --light-color: #f8fafc;
-        }
-        `}
-      </style> */}
+      <Social name="Breno" />
     </>
   );
 }

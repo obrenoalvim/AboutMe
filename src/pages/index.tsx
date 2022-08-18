e5b8fca6dd51b7a2e5f435e4694308a345e40078
Breno Alvim <alvimbrenosilva6@gmail.com>
2022-08-18 13:37:15 -0300

    add file in root global.ts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,11 +18,14 @@ import Work from '../components/Work/index'
 
 import Footer from '../components/Footer/index'
 
+import GlobalStyle from './global'
+
 export default function Home() {
   return (
     <>
       <Meta />
       <Link />
+      <GlobalStyle />
       <Title name="Breno Alvim" language="EUA" />
       <Header language="EUA" />
       <div className="hr" id="Home" />
@@ -36,43 +39,6 @@ export default function Home() {
       <Work language="EUA" />
       <div className="hr" id="Footer" />
       <Footer language="EUA" />
-      <style jsx global>
-        {`
-          * {
-            scroll-behavior: smooth;
-          }
-          body {
-            margin: 0;
-            padding: 0;
-            background-color: var(--brand-color);
-          }
-          :root {
-            --brand-color: #0f172a;
-            --dark-color: #152d73;
-            --mid-color: #cbd5e1;
-            --light-color: #f8fafc;
-            --green-color: #5ae4a8;
-            --grey-color: #2c3342;
-            --surface-color: #fff;
-            --pattern-font: 'Inter', sans-serif;
-          }
-          body::-webkit-scrollbar {
-            width: 12px; /* width of the entire scrollbar */
-          }
-
-          body::-webkit-scrollbar-track {
-            background: var(--brand-color); /* color of the tracking area */
-          }
-
-          body::-webkit-scrollbar-thumb {
-            background-color: var(
-              --brand-color
-            ); /* color of the scroll thumb */
-            border-radius: 20px; /* roundness of the scroll thumb */
-            border: 3px solid var(--brand-color); /* creates padding around scroll thumb */
-          }
-        `}
-      </style>
     </>
   )
 }

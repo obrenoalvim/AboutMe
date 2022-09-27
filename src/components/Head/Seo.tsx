e2b7ce6e1cbd1668b00ddefd5d4135c12b949e04
Breno Alvim <alvimbrenosilva6@gmail.com>
2022-09-27 11:46:10 -0300

    change SEO component, type content card

--- src/components/Head/Seo.tsx
+++ src/components/Head/Seo.tsx
@@ -10,11 +10,15 @@ export default function Meta(props: SEOProps) {
   return (
     <>
       <Head>
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@obrenoalvim" />
         <meta name="twitter:title" content={props.title} />
-        <meta name="twitter:description" content={props.description} />
+
+        <meta name="twitter:site" content="@obrenoalvim" />
+
         <meta name="twitter:image" content={props.url_image} />
+
+        <meta name="twitter:card" content="summary" />
+
+        <meta name="twitter:description" content={props.description} />
       </Head>
     </>
   )
--- src/components/Head/Title.tsx
+++ src/components/Head/Title.tsx
@@ -1,26 +1,16 @@
 interface TitleProps {
-  name: string,
+  name: string
   language: string
 }
 
 import Head from 'next/head'
 
 export default function Title(props: TitleProps) {
-  if (props.language == 'Br') {
-    return (
-      <>
-        <Head>
-          <title>{props.name}</title>
-        </Head>
-      </>
-    )
-  } else {
-    return (
-      <>
-        <Head>
-          <title>{props.name}</title>
-        </Head>
-      </>
-    )
-  }
+  return (
+    <>
+      <Head>
+        <title>{props.name}</title>
+      </Head>
+    </>
+  )
 }

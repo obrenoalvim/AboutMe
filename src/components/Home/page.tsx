a1c1226caa0390bb6b48827f25e8c2bd338aeaa4
Breno Alvim <alvimbrenosilva6@gmail.com>
2023-08-08 22:57:01 -0300

    style: started responsivity home componente

--- src/components/Home/page.tsx
+++ src/components/Home/page.tsx
@@ -6,42 +6,44 @@ export default function HomePage() {
   return (
     <Container>
       <Main>
-        <span className="AboutMe">
-          Hello, World ! {`I'm`} <strong>Breno Alvim</strong>
-        </span>
-        <span>Full-Stack</span>
-        <h1 className="glitch" data-glitch="Developer">
-          Developer
-        </h1>
-        <div className="icons">
-          <Link href={`https://tabnews.com.br/obrenoalvim`}>
-            <Image
-              src="/svg/tabnews.svg"
-              width={50}
-              height={50}
-              alt="TESTE"
-              className="icon"
-            />
-          </Link>
-          <Link href={`https://github.com/obrenoalvim`}>
-            <Image
-              src="/svg/github.svg"
-              width={50}
-              height={50}
-              alt="TESTE"
-              className="icon"
-            />
-          </Link>
-          <Link href={`https://linkedin.com/in/brenoalvim`}>
-            <img
-              src="/svg/linkedin.svg"
-              width={50}
-              height={50}
-              alt="TESTE"
-              className="icon"
-            />
-          </Link>
-        </div>
+        <main>
+          <span className="AboutMe">
+            Hello, World ! {`I'm`} <strong>Breno Alvim</strong>
+          </span>
+          <span>Full-Stack</span>
+          <h1>
+            Developer
+          </h1>
+          <div className="icons">
+            <Link href={`https://tabnews.com.br/obrenoalvim`}>
+              <Image
+                src="/svg/tabnews.svg"
+                width={50}
+                height={50}
+                alt="TabNews"
+                className="icon"
+              />
+            </Link>
+            <Link href={`https://github.com/obrenoalvim`}>
+              <Image
+                src="/svg/github.svg"
+                width={50}
+                height={50}
+                alt="Github"
+                className="icon"
+              />
+            </Link>
+            <Link href={`https://linkedin.com/in/brenoalvim`}>
+              <Image
+                src="/svg/linkedin.svg"
+                width={50}
+                height={50}
+                alt="Linkedin"
+                className="icon"
+              />
+            </Link>
+          </div>
+        </main>
       </Main>
     </Container>
   );
--- src/components/Home/styles.ts
+++ src/components/Home/styles.ts
@@ -9,18 +9,30 @@ export const Container = styled.div`
 export const Main = styled.div`
   width: 100%;
   height: 100%;
+  max-width: 1920px;
+  max-height: 1080px;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   text-align: left;
   margin: 0 auto;
+  box-sizing: content-box;
+
+  main {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+    object-fit: contain;
+    border: 1px solid red;
+  }
 
   .AboutMe {
     width: 56.5%;
     text-align: left;
     color: #5ae4a8;
-    font-size: 2.25rem;
+    font-size: 2.25vw;
     font-style: normal;
     font-weight: 200;
     line-height: normal;
@@ -30,7 +42,7 @@ export const Main = styled.div`
     width: 57%;
     text-align: left;
     color: #f2f2f2;
-    font-size: 8rem;
+    font-size: 7vw;
     font-style: normal;
     font-weight: 400;
     line-height: normal;
@@ -38,7 +50,7 @@ export const Main = styled.div`
 
   h1 {
     color: #f2f2f2;
-    font-size: 15.625rem;
+    font-size: 13vw;
     font-style: normal;
     font-weight: 700;
     line-height: normal;
@@ -49,7 +61,7 @@ export const Main = styled.div`
     width: 57.8%;
     text-align: right;
     color: #5ae4a8;
-    font-size: 2.25rem;
+    font-size: 2vw;
     font-style: normal;
     font-weight: 200;
     line-height: normal;
@@ -58,5 +70,23 @@ export const Main = styled.div`
     align-items: center;
     gap: 15px;
     color: #5ae4a8;
+  }
+
+  @media (min-width: 1920px) {
+    .AboutMe {
+      font-size: 2.25rem;
+    }
+
+    span {
+      font-size: 8rem;
+    }
+
+    h1 {
+      font-size: 15.625rem;
+    }
+
+    .icons {
+      font-size: 2.25rem;
+    }
   }
 `;
